91b666802834922b6b0c7b6a37fa3b31cf4f6439
Chris Treadway <chris.treadway@radiant.digital>
2025-04-23 14:05:09 -0600

    comment

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
 import {
 	Image,
 	StyleSheet,
-	Platform,
+	// Platform,
 	ScrollView,
 	View,
 	ImageBackground,
 	Text,
 } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
+// import { HelloWave } from "@/components/HelloWave";
+// import ParallaxScrollView from "@/components/ParallaxScrollView";
+// import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 import * as React from "react";
